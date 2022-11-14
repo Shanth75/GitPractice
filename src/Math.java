a676dc85187b3676b77eaa04d80fc6b06f325ff2
shanth75 <shanker07@hotmail.com>
2022-11-14 16:34:01 -0600

    a

---
+++ src/Math.java
@@ -0,0 +1,13 @@
+public class Math {
+    public static void main(String[] args) {
+        int a=5;
+        int b=10;
+        int sum=a+b;
+        int times=a*b;
+        int minus=b-a;
+
+        System.out.println(sum);
+        System.out.println(times);
+        System.out.println(minus);
+    }
+}
